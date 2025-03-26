193a3ba92849b4e931c3fd8d4bdb75cd9d64465a
issam-mhj <issammahtaj02@gmail.com>
2025-03-26 10:20:46 +0000

    QUIC-9 <add migrations>

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -17,6 +17,8 @@ return new class extends Migration
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->boolean('is_suspended')->default(false);
+            $table->enum('role', ['user', 'provider', 'admin'])->default('user');
             $table->rememberToken();
             $table->timestamps();
         });
