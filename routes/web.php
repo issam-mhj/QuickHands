43b3b937b95da8000705475c6a4165aa1997742d
issam-mhj <issammahtaj02@gmail.com>
2025-03-27 09:39:51 +0000

    QUIC-2 <Front-end home page>

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\VisitController;
 use Illuminate\Support\Facades\Route;
 
-Route::view('/{any?}', 'app')->where('any', '.*');
+Route::get("/",[VisitController::class,"home"]);
